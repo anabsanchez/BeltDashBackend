26af2d6bac1801386cc33b18a3a2144733528b91
Ana <Ana@DESKTOP-H1UV899>
2025-05-09 21:47:44 +0200

    feat: Add AutoMapper mapping profile for user, role, and score

---
+++ BeltDash.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BeltDash.Application.DTOs.Auth;
+using BeltDash.Application.DTOs.Role;
+using BeltDash.Application.DTOs.Score;
+using BeltDash.Application.DTOs.User;
+using BeltDash.Domain.Entities;
+
+namespace BeltDash.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role!.Name));
+
+            CreateMap<UpdateUserDto, User>();
+
+            CreateMap<Role, RoleDto>();
+
+            CreateMap<Score, ScoreDto>()
+                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User!.Username));
+
+            CreateMap<CreateScoreDto, Score>();
+        }
+    }
+}
